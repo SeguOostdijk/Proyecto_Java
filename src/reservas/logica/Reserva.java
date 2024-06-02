dabc5ce67736ad2beccc42bccdf28c7bf83c6676
Seguoostdijk <seguoostdijk730@gmail.com>
2024-06-02 18:50:25 -0300

    agrega clase Reserva

---
+++ src/reservas/logica/Reserva.java
@@ -0,0 +1,54 @@
+package reservas.logica;
+
+public class Reserva {
+  private static int numCodigo=0;
+  private final int CODIGO=numCodigo;
+  private String fecha;
+  private String horaInicio;
+  private String horaFin;
+  private Reservable reservable;
+
+  public Reserva(String fecha, String horaInicio, String horaFin, Reservable reservable) {
+    numCodigo++;
+    this.fecha = fecha;
+    this.horaInicio = horaInicio;
+    this.horaFin = horaFin;
+    this.reservable = reservable;
+  }
+
+  public int getCODIGO() {
+    return CODIGO;
+  }
+
+  public String getFecha() {
+    return fecha;
+  }
+
+  public void setFecha(String fecha) {
+    this.fecha = fecha;
+  }
+
+  public String getHoraInicio() {
+    return horaInicio;
+  }
+
+  public void setHoraInicio(String horaInicio) {
+    this.horaInicio = horaInicio;
+  }
+
+  public String getHoraFin() {
+    return horaFin;
+  }
+
+  public void setHoraFin(String horaFin) {
+    this.horaFin = horaFin;
+  }
+
+  public Reservable getReservable() {
+    return reservable;
+  }
+
+  public void setReservable(Reservable reservable) {
+    this.reservable = reservable;
+  }
+}
