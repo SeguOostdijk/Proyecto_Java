f9d26a88fe8fce77bc7ce81a06c7e1b4fc88b59d
FranciscoViera <Franciscoviera2003@gmial.com>
2024-06-06 19:47:37 -0300

    Agrega clase main

---
+++ src/reservas/logica/Main.java
@@ -0,0 +1,11 @@
+package reservas.logica;
+
+public class Main {
+    public static void CargaDatosXML(){
+
+    }
+    public static void main(String[] args) {
+        Main.CargaDatosXML();
+
+    }
+}
