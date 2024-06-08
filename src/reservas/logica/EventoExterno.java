cacd3b4524a09bad4546137232f73fd5190ead29
Bautista Nielsen <bautistanielsen2003@hotmail.com>
2024-06-08 18:37:29 -0300

    agrega nombreOrganizacion junto con set y get

--- src/reservas/logica/EventoExterno.java
+++ src/reservas/logica/EventoExterno.java
@@ -5,10 +5,12 @@ import java.time.LocalTime;
 
 public class EventoExterno extends Evento{
     private float costoAlquiler;
+    String nombreOrganizacion;
 
-    public EventoExterno(String codigoIdentificador, int cantidadInscriptos, LocalTime horaInicio, LocalTime horaFin, String descripcion, LocalDate fechaInicio, float costoAlquiler){
+    public EventoExterno(String codigoIdentificador, int cantidadInscriptos, LocalTime horaInicio, LocalTime horaFin, String descripcion, LocalDate fechaInicio, float costoAlquiler,String nombreOrganizacion){
         super(codigoIdentificador,cantidadInscriptos,horaInicio,horaFin,descripcion,fechaInicio);
         this.costoAlquiler=costoAlquiler;
+        this.nombreOrganizacion=nombreOrganizacion;
     }
 
     public float getCostoAlquiler() {
@@ -17,5 +19,13 @@ public class EventoExterno extends Evento{
 
     public void setCostoAlquiler(float costoAlquiler) {
         this.costoAlquiler = costoAlquiler;
+    }
+
+    public String getNombreOrganizacion() {
+        return nombreOrganizacion;
+    }
+
+    public void setNombreOrganizacion(String nombreOrganizacion) {
+        this.nombreOrganizacion = nombreOrganizacion;
     }
 }
