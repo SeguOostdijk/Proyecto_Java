07103bc9ba838b6a829c2a2919a8dc7bf60afb3c
Bautista Nielsen <bautistanielsen2003@hotmail.com>
2024-06-08 18:05:31 -0300

    crea clase

--- src/reservas/logica/EventoExterno.java
+++ src/reservas/logica/EventoExterno.java
@@ -1,7 +1,21 @@
 package reservas.logica;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class EventoExterno extends Evento{
-    private int costoAlquiler;
+    private float costoAlquiler;
 
+    public EventoExterno(String codigoIdentificador, int cantidadInscriptos, LocalTime horaInicio, LocalTime horaFin, String descripcion, LocalDate fechaInicio, float costoAlquiler){
+        super(codigoIdentificador,cantidadInscriptos,horaInicio,horaFin,descripcion,fechaInicio);
+        this.costoAlquiler=costoAlquiler;
+    }
 
+    public float getCostoAlquiler() {
+        return costoAlquiler;
+    }
+
+    public void setCostoAlquiler(float costoAlquiler) {
+        this.costoAlquiler = costoAlquiler;
+    }
 }
