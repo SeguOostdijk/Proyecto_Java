a6c50e243d17ecf85f5236a678da672565646a66
pablomarco01 <pablomarcodelpont01@gmail.com>
2024-06-13 19:17:00 -0300

    main

--- src/reservas/logica/Main.java
+++ src/reservas/logica/Main.java
@@ -5,18 +5,13 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
-
-
 import java.io.File;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalTime;
-
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-
-
 
 public class Main {
     public static void CargaDatosXML(Universidad uni) {
